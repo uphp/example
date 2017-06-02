191a47ba7448edd1fb44b5b39110a306357d007b
Diego Bentes <diegopbentes@gmail.com>
2017-06-02 19:32:13 -0300

    Add config folder and initializers

---
+++ config/initializers/inflection.php
@@ -0,0 +1,8 @@
+<?php
+    namespace config\initializers;
+    use src\Inflection;
+
+    //Definição das irregularidades do inflection pelo usuário
+    Inflection::irregular([
+        "papel" => "papeis"
+    ]);
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     require("vendor/autoload.php");
 
-    $app = new Uphp\web\Application();
+    $app = new \Uphp\web\Application();
     $app->start();
+
+    echo src\Inflection::pluralize("papel");
 
     echo 1/0;
